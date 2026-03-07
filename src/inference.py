671baf5888afee16b6071938440ee69ffc9f8eb4
therisbh <therishabhgupta1@gmail.com>
2026-03-07 18:52:45 +0530

    trie

--- src/inference.py
+++ src/inference.py
@@ -30,7 +30,7 @@ def parse_arguments():
                         choices=['sgd', 'momentum', 'nag', 'rmsprop'])
     parser.add_argument('-l', '--loss', type=str, default='cross_entropy',
                         choices=['cross_entropy', 'mse'])
-    parser.add_argument('-nhl', '--num_layers', type=int, default=3)
+    parser.add_argument('-nhl', '--num_layers', type=int, default=5)
     parser.add_argument('-sz', '--hidden_size', type=int, nargs='+', default=128)
     parser.add_argument('-a', '--activation', type=str, default='relu',
                         choices=['sigmoid', 'tanh', 'relu'])
